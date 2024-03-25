12013183f54e8011eda77c19d649557ed1f6ffc4
DiegoreNshore <diego.gutierrez@improving.com>
2024-03-25 12:41:39 -0600

    Add global media queries

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+	css: {
+		preprocessorOptions: {
+			includePaths: ['node_modules'],
+			scss: {
+				additionalData: `@import "@thcode-society/thc-library/dist/mixins/ui-mediaquieries.scss";`
+			}
+		}
+	},
 })
