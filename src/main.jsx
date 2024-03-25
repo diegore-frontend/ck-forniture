32a883c485e0498cac37ef1d4a28cab149f5f6c8
DiegoreNshore <diego.gutierrez@improving.com>
2024-03-25 12:42:08 -0600

    Test CL global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+// THC Component Library
+import "@thcode-society/thc-library/dist/style.css";
+
+// Local Global styling
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
